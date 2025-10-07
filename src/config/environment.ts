623c9f99fef42cd6496b258a828f8d92321930de
matthieu.marin <matthieu.marin@renault.com>
2025-10-07 16:38:31 +0200

    small fix

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -18,7 +18,6 @@ function validateConfig(): Config {
     TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
   };
 
-  // Vérifier les variables obligatoires
   for (const [key, value] of Object.entries(requiredEnvVars)) {
     if (!value) {
       throw new Error(`Missing required environment variable: ${key}`);
@@ -35,7 +34,7 @@ function validateConfig(): Config {
     cors: {
       allowedOrigins: process.env.ALLOWED_ORIGINS
         ? process.env.ALLOWED_ORIGINS.split(",")
-        : ["https://frontend-service-782869810736.europe-west1.run.app"],
+        : ["http://localhost:3000"],
     },
     dbGateway: {
       url: process.env.DB_GATEWAY_URL || "http://localhost:3001",
