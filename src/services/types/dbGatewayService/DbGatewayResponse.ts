48ef882725b502c4597e2943d38a648679ac4d0b
Matthieu Marin <matthieumarin51@gmail.com>
2026-02-11 18:37:41 +0100

    hotfix bad gateway db

--- src/services/types/dbGatewayService/DbGatewayResponse.ts
+++ src/services/types/dbGatewayService/DbGatewayResponse.ts
@@ -1,7 +1,6 @@
 export interface DbGatewayResponse {
   id: string;
   username: string;
-  twitchUserId: string;
   profileImageUrl: string | null;
   channelDescription: string | null;
   scope: string | null;
