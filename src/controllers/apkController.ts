46b1c3fc71f381acad939c906b646a3e6688242a
Matthieu <matthieumarin51@gmail.com>
2026-05-14 16:40:24 +0200

    fix download apk

--- src/controllers/apkController.ts
+++ src/controllers/apkController.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from "express";
 import { bucketManagerService } from "../services/bucketManagerService";
-import { CustomError } from "../middlewares/errorHandler";
 
 export const getApkDownloadUrl = async (
   req: Request,
@@ -8,10 +7,6 @@ export const getApkDownloadUrl = async (
   next: NextFunction,
 ): Promise<void> => {
   try {
-    if (!req.user) {
-      return next(new CustomError("Authentication required", 401));
-    }
-
     const { url } = await bucketManagerService.getApkUrl();
 
     res.status(200).json({ success: true, url });
